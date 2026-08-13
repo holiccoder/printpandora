b5c867dca402f7f2b79cf695b2a5496972440776
sachu <sacrg793@aliyun.com>
2026-08-13 13:50:43 +0800

    Update storefront font and Pinterest verification

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="robots" content="noindex, follow">
+        <meta name="p:domain_verify" content="05cdb6e0fca5ae0f0efc2131d948432d">
 
         {{-- Default SEO fallback --}}
         <meta name="description" content="{{ config('app.name', 'InkPavo') }} - Your printing solution.">
