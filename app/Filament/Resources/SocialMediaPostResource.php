a98e097ce66625f025932cd228b856fbcab38c4f
sachu <sacrg793@aliyun.com>
2026-08-28 17:26:36 +0800

    fix: use correct Filament Actions namespace for table actions in SocialMediaPostResource

--- app/Filament/Resources/SocialMediaPostResource.php
+++ app/Filament/Resources/SocialMediaPostResource.php
@@ -154,11 +154,11 @@ class SocialMediaPostResource extends Resource
                     ]),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                Actions\EditAction::make(),
+                Actions\DeleteAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\DeleteBulkAction::make(),
+                Actions\DeleteBulkAction::make(),
             ]);
     }
 
